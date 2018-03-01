2b6e0ccb66d1c596a93c54ee8d3ea066d7537dc6
wesleybeckner <wesleybeckner@gmail.com>
2018-02-28 17:22:48 -0800

    added new theme, changed to napoleon interpreter

--- doc/conf.py
+++ doc/conf.py
@@ -41,6 +41,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon']
+    #'numpydoc']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -96,7 +97,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
