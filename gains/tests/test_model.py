7184a308448f4ddcd2cc3ff1238ba50449b175ab
wesleybeckner <wesleybeckner@gmail.com>
2018-04-27 18:16:49 -0700

    trying the keras travis yml file

--- gains/tests/test_model.py
+++ gains/tests/test_model.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function
-import gains.engine as genetic
 from gains.salt_generator import generate_solvent
 import unittest
 
@@ -10,9 +9,6 @@ class GuessIonTests(unittest.TestCase):
         target = 1300
         model_ID = "density"
         generate_solvent(target, model_ID, heavy_atom_limit=300)
-
-    def test_benchmark(self):
-        genetic.Benchmark.run(self.test_1_model)
 
 
 if __name__ == '__main__':
